fcce2f73e78e7cbee323cbb4b5bd1372f81af9d3
nahuel <nahuel_isgro12@hotmai.com>
2026-09-03 13:02:18 -0300

    cambios 2

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "Activando el entorno virtual"
+if [!"-d venv" ]; then
+    python3 -m venv venv
+fi    
 source venv/bin/activate
 
 echo "instalando depedencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
